414c129fcbda3afaec77e68972c45b25efaf2ef7
Sudhakar KT <sudhakar@tillidsoft.com>
2020-03-30 15:17:28 +0530

    Row markup update and css added.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ import DragDrop from "./DragDrop/DragDrop";
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
+      <div className="container">
+          <div className="gridRow">
+            <div className="gridCell">#</div>
+            <div className="gridCell">Task Name</div>
+            <div className="gridCell">Responsible Party</div>
+            <div className="gridCell">Last Updated</div>
+            <div className="gridCell">Doc</div>
+            <div className="gridCell">Com</div>
+            <div className="gridCell">Created</div>
+          </div>
           {
           mockItems.map((item, index) => (
             <DragDrop
@@ -20,7 +29,21 @@ class App extends React.Component {
               destionationDropTootip="to"
               onDrop={(s,d) => console.log(s, d)}
             >
-              <div className="container">
+              <div className="gridRow" key={item.id.toString()}>
+                
+                <span className="drag-top"></span>
+                <span className="drag-over">{item.name}</span>
+                <span className="drag-bottom"></span>
+                
+                <div className="gridCell">#</div>
+                <div className="gridCell"> {item.name} </div>
+                <div className="gridCell"> {item.name} </div>
+                <div className="gridCell"> {item.name} </div>
+                <div className="gridCell"> {item.name} </div>
+                <div className="gridCell"> {item.name} </div>
+                <div className="gridCell"> {item.name} </div>
+              </div>
+              {/* <div className="container">
                 <div className="row" key={item.id.toString()}>
                   <div className="col drag-item">
                     <span className="drag-item-inner">
@@ -29,7 +52,7 @@ class App extends React.Component {
                     </span>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </DragDrop>
           ))
         }
